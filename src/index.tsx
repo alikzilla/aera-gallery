87a2c00bbd024efb2cf9d59799e8a6fc60b004b7
alikzilla <nuspekovalihan@gmail.com>
2025-01-14 02:20:03 +0500

    dadsa

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import App from "./App";
 import "./index.css";
 import "./helpers/i18n";
 import reportWebVitals from "./helpers/reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
