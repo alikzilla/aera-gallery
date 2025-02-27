596aeb867472df2c281958bffd6005d63cc4a25d
alikzilla <nuspekovalihan@gmail.com>
2025-02-27 15:05:17 +0500

    added meta pixel code

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import { Homepage, AboutPage, ContactsPage, PerfumePage } from "./pages";
 import { Header, Footer } from "./components";
 import { useEffect } from "react";
+import MetaPixel from "./components/metapixel/metapixel";
 
 function App() {
   const location = useLocation();
 
   useEffect(() => {
-    gtag('event', 'page_view', {
+    gtag("event", "page_view", {
       page_path: location.pathname + location.search,
     });
   }, [location]);
@@ -15,6 +16,8 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+
+      <MetaPixel />
 
       <div className="flex-grow pt-[60px]">
         <Routes>
